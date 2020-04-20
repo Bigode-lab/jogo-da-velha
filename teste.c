008be96cb425d93a67962e3c550a64d80a28f600
douglas-lab <62803336+douglas-lab@users.noreply.github.com>
2020-04-20 18:34:55 -0300

    versão final
    adição da função diagonal
    correção de erros
    codigo limpo

--- teste.c
+++ teste.c
@@ -1,118 +1,76 @@
 #include<stdio.h>
 #define max 3
-//o programa não esta aceitando fim quando se completa uma liha com um unico caracter presente
-//program esta identificando quando nao e a linha corre
-//ou seja nao a vencedores ele continua executando o codigo ate o fim
-int resultado_linha(char v[max][max]){
+int linha(char v[max][max]){
 	int i=0, x=0, prox = 0;
 	char a;
 	for ( i = 0; i < max; i++){
-
 		a = v[i][0];
-		//printf("aqui 0 x: %d\n", x);
-		//printf("aqui 0 i:%d\n", i);
 		if(a == ' '){
 			return 0;
 		}else{
-
 			for (x = 0; x < max; x++){
-				//printf("aqui 1 x: %d\n", x);
-				//printf("aqui 1 i: %d\n", i);
-				
-				//printf("simbolo na coordenada: %c\n", v[i][x]);
-//erro na condição resolvido, quando x recebia 2 ele sai pois a condição julga o proximo ou seja x = 3 por isso não entrava no if
-				if(x+1 <= max /*|| x == 2*/){
-					//printf("aqui 2 x: %d\n", x);
-					//printf("aqui 2 i: %d\n", i);
+				if(x < max){
 					prox = v[i][x+1];
-					//printf("prox: char %c\n", prox);
-					if ((x == 2) && (v[i][1] != ' ')){
-						prox = v[i][x];
-						//printf("prox char1 %c\n", prox);
-						if(prox == a){
-						//printf("aqui 4 x: %d\n", x);
-//retun temporario 
+					if (prox == ' '){
+						x = 4;
+					}else if((x == 2) && (v[i][x] != ' ')){
 						return 1;
-					}
-						//printf("coordenada %c\n", v[i][x]);
-					}
-//if esta funcionando, porém quando chega no x=2 ele identiica como caracter o da coordenada 1,0
-					else if((prox != a) && (prox != ' ')){
-							printf("%c\n", prox);
-							printf("nao e a linha horizontal\n");
-							x = 4;
-							printf("aqui 3 x: %d\n", x);
-							//return 1;
-//erro ta na condição do else if, else if não esta chegando a ser avaliado 
-					}//else if((x == 2) && (prox == a)){
-						//printf("aqui 4 x: %d\n", x);
-//retun temporario 
-						//return 1;
-					//}
-					else{
-						printf("aqui 5\n");
-// nao pode haver returni aqui pois se nao nao e executado todas as somas do x;
-					}
-					//printf("aqui 6 prox char2 %c\n", prox);
+					}else if((prox != a) && (prox != ' ')){
+						x = 4;
+					}	
+				}else{
 				}
 			}
 		}
 	}
 }
-
-int resultado_coluna(char v[max][max]){
+int coluna(char v[max][max]){
 	int i=0, x=0, prox = 0;
 	char a;
 	for ( x = 0; x < max; x++){
-
 		a = v[0][x];
-		printf("aqui 0 x: %d\n", x);
-		printf("aqui 0 i:%d\n", i);
 		if(a == ' '){
 			return 0;
 		}else{
-
 			for (i = 0; i < max; i++){
-				printf("aqui 1 x: %d\n", x);
-				printf("aqui 1 i: %d\n", i);
-				
-				printf("simbolo na coordenada: %c\n", v[i][x]);
-				if(i+1 <= max /*|| x == 2*/){
-					printf("aqui 2 x: %d\n", x);
-					printf("aqui 2 i: %d\n", i);
+				if(i < max){
 					prox = v[i+1][x];
-					//printf("prox: char %c\n", prox);
-					if ((i == 2) && (v[1][x] != ' ')){
-						prox = v[i][x];
-						printf("prox char1 %c\n", prox);
-						if(prox == a){
-						printf("aqui 4 x: %d\n", x);
+					if (prox == ' '){
+						i = 4;
+					}else if((i == 2) && (v[i][x] != ' ')){
 						return 1;
-					}
-						printf("coordenada %c\n", v[i][x]);
-					}
-					else if((prox != a) && (prox != ' ')){
-							printf("%c\n", prox);
-							printf("nao e a coluna vertical %d\n", i);
-							i = 4;
-							printf("aqui 3 x: %d\n", i);
-							//return 1;
-					}else{
-						printf("aqui 5\n");
-					}
-					//printf("aqui 6 prox char2 %c\n", prox);
+					}else if((prox != a) && (prox != ' ')){					
+						i = 4;
+					}	
+				}else{
 				}
 			}
 		}
 	}
 }
+
+int diagonal(char v[max][max]){
+	char a, b;
+	if (v[0][0] != ' '){
+		a = v[0][0];
+	}
+	if (v[0][2] != ' '){
+		b = v[0][2];
+	}
+	if ((v[0][0] == a) && (v[1][1] == a) && (v[2][2]==a)){
+		return 1;
+	}
+	if ((v[0][2] == b) && (v[1][1] == b) && (v[2][0]== b)){
+		return 1;
+	}
+}
+
 //função velha esta correta, não a erro
 int velha(char v[max][max]){
-	int i, j, x, jogador = 1, controle =0, resultado=0;
+	int i, j, x, jogador = 1, controle = 0, resultado_coluna= 0, resultado_linha= 0;
+	int resultado_diagonal = 0 ;
 	char ch;
-	
 	printf("jogador 1: x\njogador 2: O\n");
-
 	for (int j = 0; j < 9; j++){
 
 		if(jogador == 1){
@@ -152,9 +110,10 @@ int velha(char v[max][max]){
 				printf("\n----------\n");
 			}	
 		}
-		resultado = resultado_linha(v);
-		resultado1 = resultado_coluna(v);
-		if ((resultado == 1) && (resultado1 == 1)){
+		resultado_linha = linha(v);
+		resultado_coluna = coluna(v);
+		resultado_diagonal = diagonal(v);
+		if ((resultado_linha == 1 )||(resultado_diagonal == 1)||(resultado_coluna == 1)){
 			printf("fim de jogo, vencedor jogador %d\n", jogador);
 			return 0;
 		}
